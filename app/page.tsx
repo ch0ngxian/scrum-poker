766afa99f4a09ab6c9a535d19d27f832022d0180
Chong Xian <chongxian@easystore.co>
2023-06-30 22:11:27 +0800

    remove unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { Button } from "./components/Button";
 import { Textfield } from "./components/Textfield";
 import { api } from "./lib/api";
