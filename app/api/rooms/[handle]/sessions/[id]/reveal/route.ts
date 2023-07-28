4bfda0be2dca5c04f4c81d836184b85629f79873
Chong Xian <chongxian@easystore.co>
2023-07-28 18:03:04 +0800

    save the result into session

--- app/api/rooms/[handle]/sessions/[id]/reveal/route.ts
+++ app/api/rooms/[handle]/sessions/[id]/reveal/route.ts
@@ -17,10 +17,27 @@ export async function POST(request: Request, context: { params: { handle: string
     .single();
   if (!votingSession) return NextResponse.json({ error: "Fail to retrieve voting session" }, { status: 500 });
 
-  const { data: votes } = await supabase.from("votes").select("user_id, point").eq("voting_session_id", votingSession.id);
+  let { data: votes } = await supabase.from("votes").select("user_id, point").eq("voting_session_id", votingSession.id);
+  if (!votes) votes = [];
+
+  const counts: { [key: number]: number } = {};
+
+  votes.forEach((vote) => {
+    const point = vote.point as number;
+    counts[point] = (counts[point] || 0) + 1;
+  });
+
+  const result = Object.entries(counts).map(([point, count]) => ({ point: parseInt(point), count }));
+
+  const { error } = await supabase
+    .from("voting_sessions")
+    .update({
+      result: result,
+    })
+    .eq("id", votingSession.id);
+  if (error) return NextResponse.json({ error: "Fail to save result" }, { status: 500 });
 
   return NextResponse.json({
-    average_point: votes ? votes.map((vote) => vote.point).reduce((x, y) => x + y) / votes.length : null,
-    votes: votes,
+    success: true,
   });
 }
